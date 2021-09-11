92dce9d8b5d6a79025f23da7bd311733935e6a95
Luca Marzari <luca@pop-os.localdomain>
2021-09-11 15:45:32 +0200

    changed name main

--- operations/division.py
+++ operations/division.py
@@ -27,7 +27,7 @@ def div(dividend, divisor, accumulator,c_dividend, circ, cl_index):
                          shots=10).result()
  
         counts = result.get_counts("qc")
-        print(counts)
+        #print(counts)
         c_dividend_str = list(counts.keys())[0].split()[cl_index]
 
     subtraction.sub(accumulator, d, circ)
